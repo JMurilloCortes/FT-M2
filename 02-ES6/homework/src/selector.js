0f2c64ef25d6e44ad3a3e11c8c9969ca7ddd062e
JMurilloCortes <jealmuco@gmail.com>
2023-11-09 16:53:44 -0500

    09-11-2023

--- 02-ES6/homework/src/selector.js
+++ 02-ES6/homework/src/selector.js
@@ -2,12 +2,13 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
-
   // TU CÓDIGO AQUÍ
-  if (matchFunctionMaker(startEl)) resultSet.push(startEl);
-  for (let i = 0; i < startEl.childElementCount; i++) {
-    let aux = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
-    resultSet = [...resultSet, ...aux];
+  if (typeof startEl === "undefined") {
+    startEl = document.body;
+  }
+  if (matchFunc(startEl)) resultSet.push(startEl);
+  for (const child of startEl.children) {
+    resultSet.push(...traverseDomAndCollectElements(matchFunc, child));
   }
   return resultSet;
 };
@@ -17,7 +18,9 @@ var selectorTypeMatcher = function (selector) {
   // tu código aquí
   if (selector[0] === "#") return "id";
   if (selector[0] === ".") return "class";
-  if (selector.split(".").length > 1) return "tag.class";
+  for (let i = 1; i < selector.length; i++) {
+    if (selector[i] === ".") return "tag.class";
+  }
   return "tag";
 };
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -28,12 +31,27 @@ var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = (element) => `#${element.id}` === selector;
   } else if (selectorType === "class") {
+    matchFunction = (element) => {
+      for (let i = 0; i < element.classList.length; i++) {
+        if (`.${element.classList[i]}` === selector) return true;
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
+    matchFunction = (element) => {
+      let [tag, clase] = selector.split(".");
+      let funcClass = matchFunctionMaker(`.${clase}`);
+      let funcTag = matchFunctionMaker(tag);
+      return funcClass(element) && funcTag(element);
+    };
   } else if (selectorType === "tag") {
+    matchFunction = (element) => element.tagName === selector.toUpperCase();
   }
   return matchFunction;
 };
+
 var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
--- 04-Bundlers/homework/browser/app.js
+++ 04-Bundlers/homework/browser/app.js
@@ -1,25 +1,26 @@
-(function () {
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+const whiteboard = require("./whiteboard");
 
-  socket.on("connect", function () {
-    console.log("Connected!");
-  });
+const io = require("socket.io-client");
 
-  socket.on("load", function (strokes) {
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
-  });
+var socket = io(window.location.origin);
 
-  socket.on("draw", function (start, end, color) {
+socket.on("connect", function () {
+  console.log("Connected!");
+});
+
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
+});
 
-  whiteboard.on("draw", function (start, end, color) {
-    socket.emit("draw", start, end, color);
-  });
-})();
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
